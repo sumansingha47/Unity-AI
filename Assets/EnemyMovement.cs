5baf33c9e66b138835b38a4417f09a6ba36eb600
SUMAN SINGHA <sumansingha047@gmail.com>
2024-05-30 23:02:42 +0530

    Start Working

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float fov = 120f; // fov = Field of View
+    public Transform target;
+    public bool inSight;
+
+
+    private void Update() {
+
+
+    }
+}
