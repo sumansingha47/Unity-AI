824531317c33fa40858b91ab9e0d630524f98867
SUMAN SINGHA <sumansingha047@gmail.com>
2024-06-15 00:44:18 +0530

    shooting system amost comlpete

--- Assets/Advance Ai/Scripts/FPSShoooterTut.cs
+++ Assets/Advance Ai/Scripts/FPSShoooterTut.cs
@@ -6,9 +6,11 @@ public class FPSShoooterTut : MonoBehaviour
 {
     public Camera cam;
     public GameObject projectile;
+    public GameObject muzzle;
     public Transform LHFirePoint, RHFirePoint;
     public float projectileSpeed = 30;
     public float fireRate = 4;
+    public float arcRange = 1;
 
     private Vector3 destination;
     private bool leftHand;
@@ -54,6 +56,11 @@ public class FPSShoooterTut : MonoBehaviour
     {
         var projectileObj = Instantiate(projectile, firePoint.position, Quaternion.identity) as GameObject;
         projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * projectileSpeed;
+
+        iTween.PunchPosition(projectileObj, new Vector3(Random.Range(-arcRange, arcRange), Random.Range(-arcRange, arcRange), 0), Random.Range(0.5f, 2));
+
+        var muzzleObj = Instantiate(muzzle, firePoint.position, Quaternion.identity) as GameObject;
+        Destroy(muzzleObj, 2);
 
     }
 }
--- Assets/Advance Ai/Scripts/ProjectileTut.cs
+++ Assets/Advance Ai/Scripts/ProjectileTut.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class ProjectileTut : MonoBehaviour
 {
+    public GameObject ImpactVFX;
     private bool collided;
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player" && !collided)
         {
             collided = true;
+
+            var impact = Instantiate(ImpactVFX, collision.contacts[0].point, Quaternion.identity) as GameObject;
+
+            Destroy(impact, 2);
+            
             Destroy(gameObject);
         }
     }
